2d734c50dbe36010e3a53cf5754ca6e0e68cff6b
jaemcu <cupp.j.e@gmail.com>
2019-11-19 20:42:20 -0500

    Commit w/ new HTML Board, CSS style, and messages
    new HTML board with better semantic elements
    new CSS styling to board for visual appeal
    Game UI messages for player turns addeds

--- assets/scripts/game-move/events.js
+++ assets/scripts/game-move/events.js
@@ -59,27 +59,51 @@ const winningGame = function (currentPlayer) {
   if ((cell0 === currentPlayer) && (cell1 === currentPlayer) && (cell2 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#0').css('color', 'red')
+    $('#1').css('color', 'red')
+    $('#2').css('color', 'red')
   } else if ((cell3 === currentPlayer) && (cell4 === currentPlayer) && (cell5 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#3').css('color', 'red')
+    $('#4').css('color', 'red')
+    $('#5').css('color', 'red')
   } else if ((cell6 === currentPlayer) && (cell7 === currentPlayer) && (cell8 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#6').css('color', 'red')
+    $('#7').css('color', 'red')
+    $('#8').css('color', 'red')
   } else if ((cell0 === currentPlayer) && (cell4 === currentPlayer) && (cell8 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#0').css('color', 'red')
+    $('#4').css('color', 'red')
+    $('#8').css('color', 'red')
   } else if ((cell2 === currentPlayer) && (cell4 === currentPlayer) && (cell6 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#2').css('color', 'red')
+    $('#4').css('color', 'red')
+    $('#6').css('color', 'red')
   } else if ((cell0 === currentPlayer) && (cell3 === currentPlayer) && (cell6 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#0').css('color', 'red')
+    $('#3').css('color', 'red')
+    $('#6').css('color', 'red')
   } else if ((cell1 === currentPlayer) && (cell4 === currentPlayer) && (cell7 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#1').css('color', 'red')
+    $('#4').css('color', 'red')
+    $('#7').css('color', 'red')
   } else if ((cell2 === currentPlayer) && (cell5 === currentPlayer) && (cell8 === currentPlayer)) {
     console.log('Winner!')
     gameOver = true
+    $('#2').css('color', 'red')
+    $('#5').css('color', 'red')
+    $('#8').css('color', 'red')
   } else {
     console.log('this is a tie or continue?')
   }
@@ -88,6 +112,7 @@ const winningGame = function (currentPlayer) {
 // this is the let player click on space and add mark variable and functions
 let player1 = 'x'
 let gameOver = false
+let moveCount = 0
 // let player2 = 'o'
 //
 
@@ -123,11 +148,25 @@ const onGameMove = event => {
   // $('.col-sm').innerHTML += 'A'
   // this is the function that prevents a spot from being clicked if it was already chosen
   const target = $(event.target)
-  console.log(target.text() + ' ' + target.is('1'))
+
   if (!(target.text() === 'x') && !(target.text() === 'o') && (gameOver === false)) {
     target.html(player1)
+    moveCount++
     winningGame(player1)
+    if (gameOver === true) {
+      $('#winner-announcement').html('Player <strong>' + player1 + '</strong> won!')
+      $('#winner-announcement').css('color', 'red')
+    }
     changePlayer()
+    if ((gameOver === false) && (moveCount < 9)) {
+      $('#winner-announcement').html('Player <strong>' + player1 + '</strong>, your move!')
+    } else if (gameOver === false) {
+      gameOver = true
+      $('#winner-announcement').html('It\'s a tie!')
+      $('#winner-announcement').css('color', 'blue')
+    }
+  } else if (gameOver === false) {
+    $('#winner-announcement').html('Player <strong>' + player1 + '</strong>, choose another spot!')
   }
   // api.gameMove(formData)
   //   .then(ui.onGameMoveSuccess)
@@ -148,7 +187,7 @@ const onGameMove = event => {
 
 
 const addHandlers = event => {
-  $('.col-sm').on('click', onGameMove)
+  $('.cells').on('click', onGameMove)
 }
 
 // Do I add this into the addHandlers to make a callback in it to see if it is functioning?
